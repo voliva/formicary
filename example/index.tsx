5d3036db313382de81c4122c9873cb636648bee6
Víctor Oliva <olivarra1@gmail.com>
2021-11-07 20:35:06 +0100

    add working example

--- example/index.tsx
+++ example/index.tsx
@@ -15,20 +15,29 @@ import {
   useIsValid,
   setFieldError,
   useControl,
+  FormicaryContext,
+  createKeyFn,
 } from ".././src";
 import { useState } from "react";
 import { useEffect } from "react";
 
 interface FormValue {
-  min: number;
-  max: number;
-  bar: string;
+  min: string;
+  max: string;
+  subcontrol: string;
 }
 
-// const key = createKeyFn<FormValue>();
+const key = createKeyFn<FormValue>();
 
 const Form = () => {
-  const form = useForm<FormValue>();
+  const form = useForm<FormValue>({
+    initialValue: {
+      min: "1",
+      max: "5",
+      subcontrol: "subcontrol value",
+    },
+  });
+
   const errors = useErrors(form);
   const minField = useInput(form, {
     initialValue: "0",
@@ -50,34 +59,38 @@ const Form = () => {
   }, []);
 
   return (
-    <div>
-      <input name="min" placeholder="min" ref={minField} />
-      {showMax && <input name="max" placeholder="max" ref={maxField} />}
-      <div>minErrors: {errors.min}</div>
-      <div>maxErrors: {errors.max}</div>
-      <div>Minimum: {min}</div>
-      <div>{pristine ? "pristine" : "dirty"}</div>
-      <div>{isValid ? "valid" : "invalid"}</div>
-      {pristine ? null : <SubComponent formRef={form} />}
-      <button
-        onClick={() => {
-          const value = readForm(form);
-          console.log(value);
-          setFieldError(form, "max", [
-            `value ${value.max} is invalid (trololo)`,
-          ]);
-        }}
-      >
-        Submit
-      </button>
-    </div>
+    <FormicaryContext.Provider value={form}>
+      <div>
+        <input name="min" placeholder="min" ref={minField} />
+        {showMax && <input name="max" placeholder="max" ref={maxField} />}
+        <div>minErrors: {errors.min}</div>
+        <div>maxErrors: {errors.max}</div>
+        <div>Minimum: {min}</div>
+        <div>{pristine ? "pristine" : "dirty"}</div>
+        <div>{isValid ? "valid" : "invalid"}</div>
+        {pristine ? null : <SubComponent />}
+        <button
+          onClick={() => {
+            const value = readForm(form);
+            console.log(value);
+            setFieldError(form, "max", [
+              `value ${value.max} is invalid (trololo)`,
+            ]);
+          }}
+        >
+          Submit
+        </button>
+      </div>
+    </FormicaryContext.Provider>
   );
 };
 
-const SubComponent = ({ formRef }) => {
-  const control = useControl(formRef, {
+const SubComponent = () => {
+  const control = useControl({
     initialValue: "something",
-    key: "subcontrol",
+    // without `keyFn` this shows an error of "s" | "s.${string}". It's annoying.
+    // it should just accept it and return unknown on that case...
+    key: key("subcontrol"),
   });
 
   return <div>{control.value}</div>;
