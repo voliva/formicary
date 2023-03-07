a23f6c13862aa20917345e68856add01b9d83a3b
Víctor Oliva <olivarra1@gmail.com>
2023-03-07 22:11:31 +0100

    add tests for unregister feature

---
+++ test/unregister.test.tsx
@@ -0,0 +1,98 @@
+import "@testing-library/jest-dom/extend-expect";
+import { act, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import {
+  FormRef,
+  isAtMost,
+  isNumber,
+  pipeValidators,
+  useErrors,
+  useForm,
+  useInput,
+  useIsPristine,
+} from "../src";
+
+interface FormData {
+  min: number;
+  max: number;
+}
+
+const RemovableInput = ({ form }: { form: FormRef<FormData> }) => {
+  const ref = useInput(form, (v) => v.min, {
+    initialValue: "0",
+    validator: pipeValidators(isNumber(), isAtMost("max")),
+    unregisterOnUnmount: true,
+  });
+
+  return <input placeholder="min" ref={ref} />;
+};
+
+const Form = ({ withMin }: { withMin?: boolean }) => {
+  const form = useForm<FormData>();
+  const errors = useErrors(form);
+  const isPristine = useIsPristine(form);
+
+  return (
+    <div>
+      {withMin && <RemovableInput form={form} />}
+      <input
+        placeholder="max"
+        name="max"
+        ref={useInput(form, (v) => v.max, {
+          initialValue: "10",
+          validator: isNumber(),
+        })}
+      />
+      <div data-testid="errors">{Object.keys(errors).join(" ")}</div>
+      <div data-testid="pristine">{isPristine ? "yes" : "no"}</div>
+    </div>
+  );
+};
+
+describe("unregister", () => {
+  it("removes the error state", async () => {
+    const { getByPlaceholderText, getByTestId, rerender } = render(
+      <Form withMin />
+    );
+
+    expect(getByTestId("errors")).toHaveTextContent("");
+
+    userEvent.type(getByPlaceholderText("min"), "12");
+    userEvent.tab();
+
+    // Wait for error to propagate
+    await act(async () => {});
+
+    expect(getByTestId("errors")).toHaveTextContent("min");
+
+    rerender(<Form withMin={false} />);
+
+    // Wait for error to propagate
+    await act(async () => {});
+
+    expect(getByTestId("errors")).toHaveTextContent("");
+  });
+
+  it("removes the pristine state", async () => {
+    const { getByPlaceholderText, getByTestId, rerender } = render(
+      <Form withMin />
+    );
+
+    expect(getByTestId("pristine")).toHaveTextContent("yes");
+
+    userEvent.type(getByPlaceholderText("min"), "5");
+    userEvent.tab();
+
+    await act(async () => {});
+
+    expect(getByTestId("pristine")).toHaveTextContent("no");
+
+    rerender(<Form withMin={false} />);
+
+    // Wait for error to propagate
+    await act(async () => {});
+
+    expect(getByTestId("pristine")).toHaveTextContent("yes");
+  });
+});
