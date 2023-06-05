7c0b552866412461f6062b77e389c5e88f13d9ff
Víctor Oliva <olivarra1@gmail.com>
2023-06-06 00:01:58 +0200

    upgrade remaining dependencies

--- test/unregister.test.tsx
+++ test/unregister.test.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
-import { act, render } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 import {
